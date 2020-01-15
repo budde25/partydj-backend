4dcfcee44b6ef95a80f9254e0fe4eabd7aa91ac4
budde25 <budde25@gmail.com>
2020-01-14 23:22:00 -0600

    implemented adding a song

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -22,11 +22,11 @@ export const generateRoom = functions.https.onCall(async (data, context) => {
         accessToken: accessToken
     });
 
-    let playlistUri;
+    let playlistId;
 
     try {
         const response = await spotifyApi.createPlaylist(username, appName + ':' + roomCode, { 'public' : true });
-        playlistUri = response.body.uri;
+        playlistId = response.body.id;
     } catch (error) {
         console.error(error);
         return {
@@ -40,7 +40,7 @@ export const generateRoom = functions.https.onCall(async (data, context) => {
     // create the room
     try {
         await admin.firestore().collection('rooms')
-            .doc(roomCode).set({ 'enabled': true, 'owner': username, 'playlistUri':playlistUri});
+            .doc(roomCode).set({ 'enabled': true, 'owner': username, 'playlistId':playlistId});
         return {
             status: 'success',
             roomCode: roomCode,
@@ -91,7 +91,109 @@ export const closeRoom = functions.https.onCall( async (data, context) => {
  * @param song an object repesenting the song data
  * @returns bool if success
  */
-export const addSong = functions.https.onCall((data, context) => {null})
+export const addSong = functions.https.onCall( async (data, context) => {
+    const roomCode = data.roomCode;
+    const accessToken = data.accessToken;
+    const songUri = data.songUri;
+
+     // create the spotify playlist
+     const spotifyApi = new SpotifyWebApi({
+        accessToken: accessToken
+    });
+
+    // get the playlist uri from firestore
+    let playlistId;
+    try {
+        const docRef = await admin.firestore().collection('rooms')
+            .doc(roomCode).get();
+        const response = docRef.data();
+        
+        if (response !== undefined) playlistId = response.playlistId;
+        else throw new Error('document refrence is undefined');
+    }
+    catch (error) {
+        console.error(error);
+        return {
+            staus: 'error',
+            code: 401,
+            message: 'Firestore connection failed',
+        };
+    }
+
+    // add the track to the playlist
+    console.log(playlistId);
+    try {
+        await spotifyApi.addTracksToPlaylist(playlistId, [songUri]);
+    } catch (error) {
+        console.error(error);
+        return {
+            staus: 'error',
+            code: 401,
+            message: 'Spotify connection failed',
+        };
+    }
+
+    // ask spotify for the track details
+    return updateDatabase(roomCode, playlistId, spotifyApi);
+
+})
+
+async function updateDatabase(roomCode:string, playlistId: string, spotifyApi : SpotifyWebApi) {
+    console.log('updateDatabase');
+        let spotifyTracks;
+    try {
+        const response = await spotifyApi.getPlaylistTracks(playlistId);
+        spotifyTracks = response.body.items
+    } catch (error) {
+        console.error(error);
+        return {
+            staus: 'error',
+            code: 401,
+            message: 'Spotify connection failed',
+        };
+    }
+
+    const tracks = []
+    for (const item of spotifyTracks){
+        const addedBy = item.added_by;
+        const track = item.track;
+        const name = track.name;
+        const uri = track.uri;;
+        const imageUrl = track.album.images[0].url;
+        const artist = track.artists[0].name;
+        const position = track.track_number;
+        
+        // add the data to a JSON object
+        const song = {
+            'name': name,
+            'uri': uri,
+            'artist': artist,
+            'imageUrl': imageUrl,
+            'addedBy': addedBy,
+            'position': position,
+        }
+        tracks.push(song);
+    }
+    const tracksObject = { 'songs' : tracks };
+
+    console.log('push to fire');
+    // push to the firestore
+    try {
+        await admin.firestore().collection('rooms')
+            .doc(roomCode).set(tracksObject);
+        return {
+            status: 'success'
+        }
+    }
+    catch (error) {
+        console.error(error);
+        return {
+            staus: 'error',
+            code: 401,
+            message: 'Firestore connection failed',
+        };
+    }
+}
 
 /**
  * Checks if a room is open
